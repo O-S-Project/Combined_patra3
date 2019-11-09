1f2944b55602dc112c8c7bde061814072fef3a7d
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2019-11-10 04:33:33 +0530

    Spellcheck recommendation added

--- Patra2/modularfileclass/Spellcheck1.py
+++ Patra2/modularfileclass/Spellcheck1.py
@@ -1,20 +1,30 @@
 import tkinter as Tkinter
+from difflib import get_close_matches # for spelling recommendation
+
 
 # add thread
 class Spellcheck:
     """docstring for Spellcheck."""
 
-    def __init__(self, text):
+    # def __init__(self, text):
+    #     # super(Spellcheck, self).__init__()
+    #     self.text = text
+    #     self._words=open("/usr/share/dict/words").read().split("\n")
+    #
+    #     self.text.tag_configure("misspelled", foreground="red", underline=True)
+    #     #highlight it?
+    #
+    #     self.functions_binding_key()
+
+    def __init__(self, text,statusbar=None):
         # super(Spellcheck, self).__init__()
         self.text = text
         self._words=open("/usr/share/dict/words").read().split("\n")
 
         self.text.tag_configure("misspelled", foreground="red", underline=True)
         #highlight it?
-
+        self.statusbar = statusbar
         self.functions_binding_key()
-
-
 
 
     def functions_binding_key(self):
@@ -37,3 +47,28 @@ class Spellcheck:
             self.text.tag_remove("misspelled", index, "%s+%dc" % (index, len(word)))
         else:
             self.text.tag_add("misspelled", index, "%s+%dc" % (index, len(word)))
+            # sending word recommendation
+            # print(self.text[index:])
+            print(self.text.get(index,Tkinter.END))
+            str11 = self.text.get(index,Tkinter.END).split("\n") #get last word
+            print("put this in exception")
+            print(str11[0]) #check incorrect word
+            print("put this in exception")
+            # self.statusbar.update_status1(str11[0])
+            word = str11[0]
+            patterns = self._words
+            wordrec = get_close_matches(word, patterns)
+            # wordrec = closeMatches(patterns, word)
+            self.statusbar.update_status1(wordrec)
+
+
+
+
+
+
+
+
+
+
+
+#chedk
